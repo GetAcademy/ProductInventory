20da9b9b0617d50c1bf9b4aa158736aaa7a94373
GET Academy <terje@kolderup.net>
2026-08-26 09:42:27 +0200

    x

---
+++ ProductInventory.Core/ApplicationService/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using ProductInventory.Core.DomainModel;
+using ProductInventory.Core.DomainServices;
+
+namespace ProductInventory.Core.ApplicationService
+{
+    internal class ProductService(IProductRepository repo)
+    {
+        public async Task<Result<Product>> CreateProductAsync(Product product)
+        {
+            if (string.IsNullOrWhiteSpace(product.Name))
+            {
+                return Result<Product>.Failure("Navn må fylles ut.");
+            }
+
+            if (string.IsNullOrWhiteSpace(product.ProductCode))
+            {
+                return Result<Product>.Failure("Produktkode må fylles ut.");
+            }
+
+            if (product.StockCount < 0)
+            {
+                return Result<Product>.Failure("Lagerbeholdning kan ikke være negativ.");
+            }
+
+            await repo.CreateAsync(product);
+
+            return Result<Product>.Success(product);
+        }
+    }
+}
